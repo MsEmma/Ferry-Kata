32a81d482f468bc896ae24b1d08597e75e1cfe2c
MsEmma <emmaphiri@hotmail.com>
2016-07-21 16:42:23 +0200

    failing tests

--- ferry.js
+++ ferry.js
@@ -2,22 +2,47 @@ module.exports = function(maxCars, maxPeople) {
     var people_count = 0;
     var car_count = 0;
     this.cars = [];
-
+    this.carColors = {};
     this.maxCars = maxCars;
     this.maxPeople = maxPeople;
 
     this.board = function(car) {
 
-        this.cars.forEach(function(car) {
-            people_count += car.passengers;
-            car_count++;
-        });
+        people_count += car.passengers;
+        car_count++;
 
         if (people_count > this.maxPeople || car_count > this.maxCars) {
             return "rejected";
         } else {
             this.cars.push(car);
+            if (!this.carColors.hasOwnProperty(car.color)) {
+                this.carColors[car.color] = 1;
+            } else {
+                this.carColors[car.color]++;
+            }
             return "accepted";
         }
+    };
+
+    this.disembark = function(car) {
+
+        console.log(car.passengers);
+        console.log(people_count);
+        people_count -= car.passengers;
+        car_count--;
+
+        var carIndex = this.cars.indexOf(car);
+
+        if (carIndex > -1) {
+            this.cars.splice(carIndex, 1);
+        };
+    };
+
+    this.carCount = function(){
+      return car_count;
+    };
+
+    this.peopleCount = function(){
+      return people_count;
     };
 };
--- test/ferry_test.js
+++ test/ferry_test.js
@@ -1,9 +1,12 @@
 var assert = require('assert');
-var car = require('../car')
+var Car = require('../car')
 var Ferry = require('../ferry');
 
 describe('ferry kata', function() {
     var island_ferry = new Ferry(5, 20);
+    var blueCar = new car('blue', 14);
+    var redCar = new car('red', 7);
+    var yellowCar = new car('yellow', 3);
 
     it('should return 20 for maximum number of people allowed on the ferry', function() {
         var result = island_ferry.maxPeople;
@@ -16,27 +19,45 @@ describe('ferry kata', function() {
     });
 
     it('should return accepted when board method is called', function() {
-        var blueCar = new car('blue', 14);
+
         var result = island_ferry.board(blueCar);
         assert.equal(result, 'accepted');
     });
 
     it('should return accepted when board method is called', function() {
+      var island_ferry = new Ferry(5, 20);
+
         var redCar = new car('red', 7);
         var result = island_ferry.board(redCar);
+        assert.equal(island_ferry.carCount(), 1);
         assert.equal(result, 'accepted');
     });
 
     it('should return rejected when board method is called', function() {
-        var yellowCar = new car('yellow', 3);
         var result = island_ferry.board(yellowCar);
         assert.equal(result, 'rejected');
     });
 
-    it('should return the cars on board thus far', function() {
+    it('should return the car list of the cars on board the ferry', function() {
         var result = island_ferry.cars;
         assert.deepEqual(result, [
           {color: 'blue', passengers: 14 },
           {color: 'red', passengers: 7}]);
     });
+
+    it('should return a map of the car colors and their quantities', function() {
+        var result = island_ferry.carColors;
+        assert.deepEqual(result, { blue: 1, red: 1 });
+    });
+
+    // it('should return car list without the disembarked red car', function() {
+    //     island_ferry.disembark(redCar);
+    //     var result = island_ferry.cars;
+    //     assert.deepEqual(result, [{color: 'blue', passengers: 14 }]);
+    // });
+
+    // it('should return 14 after 7 red car passengers have disembarked ', function() {
+    //     var result = island_ferry.peopleCount();
+    //     assert.equal(result, 14);
+    // });
 });
